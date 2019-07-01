626919a049fac0c26e55b3083645381159c9a273
Jon Purdy <jon@jonpurdy.com>
2019-07-01 12:39:12 -0400

    fixed a few things

--- mosaicbin/feedbin.py
+++ mosaicbin/feedbin.py
@@ -11,6 +11,9 @@ except Exception as e:
 #creds = ('username@domain.net', 'password')
 verbose = True
 
+    
+UNREAD_ENTRIES_RETRIEVED = False # if True, don't go to the API
+
 track_thread_status = {}
 
     # now, if a user clicks on the title of a feed, it will redirect them to the
@@ -27,7 +30,9 @@ track_thread_status = {}
 
 
 def get_subs_and_tags():
-
+    """ Returns subscriptions with tags as a dictionary
+        Main object used by the index page
+    """
     tagging_response = get_tagging()
     if verbose:
         print(len(tagging_response))
@@ -79,9 +84,14 @@ def get_subs_and_tags():
 
     subs_dict, feeds_dict = get_subs_dict()
 
+    # adding unread counts
     for f in feeds_dict:
+    # for each feed, update the unread count
+
+        # here
 
         feeds_dict[f].unread_count = get_unread_entry_count_of_feed(feeds_dict[f].feed_id)
+        #feeds_dict[f].unread_count = 0
 
         print(feeds_dict[f].feed_id)
         print(feeds_dict[f].title)
@@ -127,6 +137,7 @@ def get_subs_dict():
         subs_dict[i['feed_id']] = i['title']
         feeds_dict[i['feed_id']] = Feed(title=i['title'], feed_id=i['feed_id'], unread_count=0)
 
+
     # for x in subs_dict:
     #     print("%s %s" % (x, subs_dict[x]))
 
@@ -134,7 +145,9 @@ def get_subs_dict():
     return subs_dict, feeds_dict
 
 
-def get_all_unread_entries():
+def get_all_unread_entries_list():
+    """ Returns a list of entry IDs that are unread
+    """
 
     path = "unread_entries.json"
     url = "%s%s" % (endpoint, path)
@@ -146,14 +159,14 @@ def get_all_unread_entries():
     return r.json()
 
 def get_unread_entries_of_feed(feed_id):
-    """ Returns a list of unread entries for a particular feed.
+    """ Returns a list of unread entries with full text for a particular feed.
     """
 
-    unread_entries_all = get_all_unread_entries()
+    unread_entries_all = get_all_unread_entries_list()
+
     unread_entries_feed = []
 
-    one_year_ago = datetime.datetime.now() - datetime.timedelta(days=365)
-    path = "feeds/%s/entries.json" % feed_id # no need for since anymore 
+    path = "feeds/%s/entries.json" % feed_id 
     url = "%s%s" % (endpoint, path)
     r = requests.get(url, auth=creds)
 
@@ -170,8 +183,7 @@ def get_unread_entry_count_of_feed(feed_id):
     """ 
 
     # override the function that gets unread entries for now
-    #unread_count = len(get_unread_entries_of_feed(feed_id))
-    unread_count = 0
+    unread_count = len(get_unread_entries_of_feed(feed_id))
 
     return unread_count
 
